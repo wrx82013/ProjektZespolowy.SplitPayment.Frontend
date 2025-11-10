eceddb4f59887f9abcebe327ba3389dabdbd24c6
gkk <kaczmarekgrzegorz11@gmail.com>
2025-11-10 22:33:07 +0100

    fixup! fixup! fixup! fixup! Fix button icon

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import {
   SplitPaymentResponseDto,
 } from "../types/api";
 
-export const API_BASE_URL = "http://projektzespolowy_splitpayment";
+export const API_BASE_URL = "http://projektzespolowy_splitpayment:5000";
 
 export const createSplitPayment = async (
   data: CreateSplitPaymentRequestDto,
